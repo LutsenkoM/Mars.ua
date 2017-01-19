4a3628b10ef0a999f8adb1b360ec48e5af5dea2d
Max <lutsenko.maxim@gmail.com>
2017-01-19 23:13:07 +0200

    1

--- wp-content/themes/Mars/header.php
+++ wp-content/themes/Mars/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <link rel="stylesheet" type="text/css" href=" <?php bloginfo('template_url') ?>/bootstrap-3.3.7-dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href=" <?php bloginfo('template_url') ?>/bootstrap-3.3.7-dist/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" type="text/css" href=" <?php bloginfo('template_url') ?>/stylesheets/screen.css"/>\
+    <link rel="stylesheet" type="text/css" href=" <?php bloginfo('template_url') ?>/stylesheets/screen.css"/>
 </head>
 
 <body>
